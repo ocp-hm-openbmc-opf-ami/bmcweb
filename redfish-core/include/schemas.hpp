70614ed496fbd8e78567974e08e247dbecf219c5
Pranoy Goru <gorumanip@ami.com>
2024-06-11 15:06:14 +0530

    Fixed Schema related failures and Added Dbus varients for REP
    Signed-off-by: Pranoy Goru <gorumanip@ami.com>

--- redfish-core/include/schemas.hpp
+++ redfish-core/include/schemas.hpp
@@ -15,7 +15,7 @@
 
 namespace redfish
 {
-    constexpr std::array<std::string_view,147> schemas {
+    constexpr std::array<std::string_view,149> schemas {
         "AmiLicenseControl"
         "AccelerationFunctionCollection",
         "AccountService",
