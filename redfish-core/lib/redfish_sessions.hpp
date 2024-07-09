fb7ebf6d3f9cd3da44c3443c2045ac42020d889a
Bharath Bharathi A <bharathbharathia@ami.com>
2024-07-09 06:10:21 +0000

    Fix for return Access Denied for Empty Username and Password

--- redfish-core/lib/redfish_sessions.hpp
+++ redfish-core/lib/redfish_sessions.hpp
@@ -676,12 +676,16 @@ inline void handleSessionCollectionPost(
     {
         if (username.empty())
         {
-            messages::propertyMissing(asyncResp->res, "UserName");
+            messages::resourceAtUriUnauthorized(asyncResp->res, req.url(),
+                                            "Invalid username ");
+
         }
 
         if (password.empty())
         {
-            messages::propertyMissing(asyncResp->res, "Password");
+            messages::resourceAtUriUnauthorized(asyncResp->res, req.url(),
+                                            "Invalid Password ");
+
         }
 
         return;
