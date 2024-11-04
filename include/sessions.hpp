7da84fc01c946d432d7dfd0c219476e25764d58a
Karthika R <rkarthika@ami.com>
2024-11-04 12:17:46 +0000

    Resolved AB_Tool Failed requests error

--- include/sessions.hpp
+++ include/sessions.hpp
@@ -353,7 +353,7 @@ class SessionStore
 
     std::vector<std::string> getAllUniqueIds()
     {
-        applySessionTimeouts();
+        //applySessionTimeouts();
         std::vector<std::string> ret;
         ret.reserve(authTokens.size());
         for (auto& session : authTokens)
