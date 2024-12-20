96ee3c0f4290b630498014c776245e376e18b20a
Prasanna S <prasannas@ami.com>
2024-12-20 14:28:10 +0000

    Fix for Identify Led Enable/Disable not working In Webui

--- redfish-core/lib/managers.hpp
+++ redfish-core/lib/managers.hpp
@@ -2403,10 +2403,10 @@ inline void handleManagersInstanceGet(
     nlohmann::json& oemIntel = oem["Intel"];
     oemIntel["@odata.type"] = "#OpenBMCManager.v1_0_0.Intel";
     oemIntel["@odata.id"] = "/redfish/v1/Managers/bmc#/Oem/Intel";
-    #if (BMCWEB_AMI_NM_MACRO)
+#if (BMCWEB_AMI_NM_MACRO)
     oemIntel["NodeManager"] = {
         {"@odata.id", "/redfish/v1/Managers/bmc/Oem/Intel/NodeManager"}};
-    #endif    
+#endif
     oem["@odata.id"] = boost::urls::format("/redfish/v1/Managers/{}#/Oem",
                                            BMCWEB_REDFISH_MANAGER_URI_NAME);
     oemOpenbmc["@odata.type"] = "#OpenBMCManager.v1_0_0.Manager";
@@ -2704,7 +2704,8 @@ inline void requestRoutesManager(App& app)
               "Oem/OpenBmc/Fan/PidControllers", pidControllers,
               "Oem/OpenBmc/Fan/Profile", profile,
               "Oem/OpenBmc/Fan/StepwiseControllers", stepwiseControllers,*/
-              "Id", vId, "TimeZoneName", timeZoneName
+              "Id", vId, "TimeZoneName", timeZoneName,
+              "LocationIndicatorActive", locationIndicatorActive
         ))
         {
             return;
@@ -2852,8 +2853,8 @@ inline void requestRoutesManagerSerialInterface(App& app)
                     BMCWEB_REDFISH_MANAGER_URI_NAME);
                 asyncResp->res.jsonValue["@odata.type"] =
                     "#SerialInterfaceCollection.SerialInterfaceCollection";
-                //asyncResp->res.jsonValue["Id"] =
-                  //  BMCWEB_REDFISH_MANAGER_URI_NAME;
+                // asyncResp->res.jsonValue["Id"] =
+                //   BMCWEB_REDFISH_MANAGER_URI_NAME;
                 asyncResp->res.jsonValue["Name"] =
                     "Serial Interface Collection";
                 asyncResp->res.jsonValue["Description"] =
