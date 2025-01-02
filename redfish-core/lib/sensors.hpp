c531866152ee3ad4fdda559b1d48802d9cea8437
J.C Chiu (邱奕偉) <jcchiu@ami.com>
2025-01-02 01:55:39 +0000

    [Revert] !243 [Revert] !182 Support PLDM sensor in Redfish

--- redfish-core/lib/sensors.hpp
+++ redfish-core/lib/sensors.hpp
@@ -417,13 +417,14 @@ void getChassis(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                 Callback&& callback)
 {
     BMCWEB_LOG_DEBUG("getChassis enter");
-    constexpr std::array<std::string_view, 2> interfaces = {
+    constexpr std::array<std::string_view, 3> interfaces = {
+        "xyz.openbmc_project.Sensor.Value",
         "xyz.openbmc_project.Inventory.Item.Board",
         "xyz.openbmc_project.Inventory.Item.Chassis"};
 
     // Get the Chassis Collection
     dbus::utility::getSubTreePaths(
-        "/xyz/openbmc_project/inventory", 0, interfaces,
+        "/", 0, interfaces,
         [callback = std::forward<Callback>(callback), asyncResp,
          chassisIdStr{std::string(chassisId)},
          chassisSubNode{std::string(chassisSubNode)}, sensorTypes](
@@ -470,7 +471,7 @@ void getChassis(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
             std::string sensorPath = *chassisPath + "/all_sensors";
             dbus::utility::getAssociationEndPoints(
                 sensorPath,
-                [asyncResp, chassisSubNode, sensorTypes,
+                [asyncResp, chassisPaths, chassisSubNode, sensorTypes,
                  callback = std::forward<const Callback>(callback)](
                     const boost::system::error_code& ec2,
                     const dbus::utility::MapperEndPoints& nodeSensorList) {
@@ -482,11 +483,19 @@ void getChassis(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                             return;
                         }
                     }
+                    dbus::utility::MapperEndPoints SensorList = nodeSensorList;
+                    for(const std::string& objpath : chassisPaths)
+                    {
+                        if(std::find(SensorList.begin(), SensorList.end(), objpath) == SensorList.end())
+                        {
+                            SensorList.emplace_back(objpath);
+                       }
+                   }
                     const std::shared_ptr<std::set<std::string>>
                         culledSensorList =
                             std::make_shared<std::set<std::string>>();
                     reduceSensorList(asyncResp->res, chassisSubNode,
-                                     sensorTypes, &nodeSensorList,
+                                     sensorTypes, &SensorList,
                                      culledSensorList);
                     BMCWEB_LOG_DEBUG("Finishing with {}",
                                      culledSensorList->size());
