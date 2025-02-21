e4784a2a867b86767bfe863bedb6b3e7b2a0605c
Nibin Chacko <nibinc@ami.com>
2025-02-21 01:51:29 +0000

    Adding Count sensors - PMT

--- redfish-core/lib/sensors.hpp
+++ redfish-core/lib/sensors.hpp
@@ -70,7 +70,8 @@ constexpr auto getSensorPaths(){
         "/xyz/openbmc_project/sensors/battery",
         "/xyz/openbmc_project/sensors/chassisstate",
         "/xyz/openbmc_project/sensors/os",
-        "/xyz/openbmc_project/sensors/watchdog"});
+        "/xyz/openbmc_project/sensors/watchdog",
+	"/xyz/openbmc_project/sensors/count/"});
     } else {
       return  std::to_array<std::string_view>({"/xyz/openbmc_project/sensors/power",
         "/xyz/openbmc_project/sensors/current",
