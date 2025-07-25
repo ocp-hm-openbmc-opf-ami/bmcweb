0d0f11c4d5e62a9c42c8d1e3d5d678cbbafe143f
Thirusabaresaan P <thirusabaresaanp@ami.com>
2025-07-25 04:06:03 +0000

    OT-13814 The PATCH update operation should not return 200 with error response

--- redfish-core/lib/redfish_sessions.hpp
+++ redfish-core/lib/redfish_sessions.hpp
@@ -863,15 +863,9 @@ inline void handleSessionServiceHead(
         boost::beast::http::field::link,
         "</redfish/v1/JsonSchemas/SessionService/SessionService.json>; rel=describedby");
 }
-inline void handleSessionServiceGet(
-    crow::App& app, const crow::Request& req,
-    const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
 
+inline void getSessionServiceInfo(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
 {
-    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
-    {
-        return;
-    }
     asyncResp->res.addHeader(
         boost::beast::http::field::link,
         "</redfish/v1/JsonSchemas/SessionService/SessionService.json>; rel=describedby");
@@ -967,6 +961,18 @@ inline void handleSessionServiceGet(
         "xyz.openbmc_project.Control.Service.SocketAttributes", "Port");
 }
 
+inline void
+    handleSessionServiceGet(crow::App& app, const crow::Request& req,
+                            const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+
+{
+    if (!redfish::setUpRedfishRoute(app, req, asyncResp))
+    {
+        return;
+    }
+    getSessionServiceInfo(asyncResp);
+}
+
 inline void handleSessionServicePatch(
     crow::App& app, const crow::Request& req,
     const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
@@ -1003,7 +1009,7 @@ inline void handleSessionServicePatch(
                         messages::internalError(asyncResp->res);
                         return;
                     }
-                    messages::success(asyncResp->res);
+                    getSessionServiceInfo(asyncResp);
                 },
                 "xyz.openbmc_project.Control.Service.Manager",
                 "/xyz/openbmc_project/control/service/bmcweb",
