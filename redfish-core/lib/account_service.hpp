314f4d4e25b6e7a74901a673008371cf0458bc24
Sam Pao (鮑世鈞) <sampao@ami.com>
2025-08-07 14:31:09 +0000

    OT-15371: [OT_2.1][Beechnutcity]There is no limit for base DN attribute in LDAP server details.

--- redfish-core/lib/account_service.hpp
+++ redfish-core/lib/account_service.hpp
@@ -3331,6 +3331,16 @@ inline void handleAccountServicePatch(
             std::variant<uint8_t>(*RememberOldPasswordTimes));
     }
 
+    if (ldapObject.baseDNList)
+    {
+        if (ldapObject.baseDNList->at(0).length() > 253 || ldapObject.baseDNList->at(0).length() < 4)
+        {
+            messages::propertyValueOutOfRange(asyncResp->res, *ldapObject.baseDNList,
+                    "BaseDistinguishedNames");
+            return;
+        }
+    }
+
     if (activeDirectoryObject.hasValue())
     {
         handleLDAPPatch(std::move(activeDirectoryObject), asyncResp,
