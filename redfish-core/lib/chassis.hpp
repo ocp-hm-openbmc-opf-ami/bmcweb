18743d3c3e490fda84317cbe82082dd9ac3b42e5
Govardhan Chenna <govardhanc@ami.com>
2025-10-09 08:55:52 +0000

    Added MinPowerWatts and MaxPowerWatts properties

--- redfish-core/lib/chassis.hpp
+++ redfish-core/lib/chassis.hpp
@@ -825,6 +825,43 @@ inline void handleChassisGetSubTree(
     messages::resourceNotFound(asyncResp->res, "Chassis", chassisId);
 }
 
+void getMinMaxValues(const std::shared_ptr<bmcweb::AsyncResp>& asyncResp)
+{
+    const std::string sensorPath =
+        "/xyz/openbmc_project/sensors/power/Platform_Power_Average_CPU1";
+    sdbusplus::asio::getAllProperties(
+        *crow::connections::systemBus,
+        "xyz.openbmc_project.IntelCPUSensor", // Service
+        sensorPath,
+        "xyz.openbmc_project.Sensor.Value", // Interface
+        [asyncResp](const boost::system::error_code& ec,
+                    const dbus::utility::DBusPropertiesMap& properties) {
+            if (ec)
+            {
+                BMCWEB_LOG_DEBUG("DBUS response error: {}",ec);
+                return;
+            }
+            const double* minValue = nullptr;
+            const double* maxValue = nullptr;
+            const bool success = sdbusplus::unpackPropertiesNoThrow(
+                dbus_utils::UnpackErrorPrinter(), properties, "MinValue",
+                minValue, "MaxValue", maxValue);
+            if (!success)
+            {
+                BMCWEB_LOG_DEBUG("Failed to unpack MinValue/MaxValue");
+                return;
+            }
+            if (minValue)
+            {
+                asyncResp->res.jsonValue["MinPowerWatts"] = *minValue;
+            }
+            if (maxValue)
+            {
+                asyncResp->res.jsonValue["MaxPowerWatts"] = *maxValue;
+            }
+        });
+}
+
 inline void handleChassisGet(App& app, const crow::Request& req,
                       const std::shared_ptr<bmcweb::AsyncResp>& asyncResp,
                       const std::string& chassisId)
@@ -857,6 +894,7 @@ inline void handleChassisGet(App& app, const crow::Request& req,
             "/xyz/openbmc_project", 0, interfaces2,
             std::bind_front(handlePhysicalSecurityGetSubTree, asyncResp));
     }
+    getMinMaxValues(asyncResp);
 }
 
 inline void
