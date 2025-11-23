19af653c77f035e6e2144e2552bfe6620e1b6925
Ranjith Kumar A <ranjithkumara@ami.com>
2025-11-23 04:48:10 +0000

    correcting error message when PATCH IPv6StaticDefaultGateways

--- redfish-core/include/utils/json_utils.hpp
+++ redfish-core/include/utils/json_utils.hpp
@@ -292,7 +292,7 @@ bool unpackValue(nlohmann::json& jsonValue, std::string_view key,
             jsonValue.get_ptr<nlohmann::json::array_t*>();
         if (arr == nullptr)
         {
-            messages::propertyValueTypeError(res, res.jsonValue, key);
+            messages::propertyValueTypeError(res, jsonValue, key);
             return false;
         }
 
