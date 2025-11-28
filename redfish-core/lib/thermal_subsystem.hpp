814d2fb928fbdb913ab568625424c8cbbe4d434d
dhanusha <dhanusha@ami.com>
2025-11-28 20:07:25 +0530

    Fix for build error

--- redfish-core/lib/thermal_subsystem.hpp
+++ redfish-core/lib/thermal_subsystem.hpp
@@ -128,7 +128,7 @@ inline void doThermalSubsystemCollection(
 
     asyncResp->res.jsonValue["Status"]["State"] = resource::State::Enabled;
     asyncResp->res.jsonValue["Status"]["Health"] = resource::Health::OK;
-    if (chassisId != "Cpld" && chassisId != "CDU")
+    if (chassisId != "Cpld" && chassisId != "CDU" && chassisId != "PowerShelf" && chassisId != "Rack")
     {
         getFanRedundancy(asyncResp, chassisId);
     }
