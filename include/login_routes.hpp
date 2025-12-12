d0977fcefa9af783794e20f9d85a6e29e2074843
GokulPrasath P <gokulp@ami.com>
2025-12-12 03:52:20 +0000

    Fix malformed error responses in account PATCH operations

--- include/login_routes.hpp
+++ include/login_routes.hpp
@@ -29,7 +29,7 @@ std::string getRole(std::string role)
     else if (role == "priv-operator")
         return "Operator";
     else if (role == "priv-user")
-        return "Readonly";
+        return "ReadOnly";
     else
         return "";
 }
