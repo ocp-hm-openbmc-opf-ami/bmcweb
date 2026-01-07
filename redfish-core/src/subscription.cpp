0c0105879c8fc67320d92dbe04660e4f67725cfa
Govardhan Chenna <govardhanc@ami.com>
2026-01-07 03:54:41 +0000

    Exception handled for SNMP trap send failure

--- redfish-core/src/subscription.cpp
+++ redfish-core/src/subscription.cpp
@@ -249,11 +249,19 @@ bool Subscription::sendSNMPTrap(uint32_t eventId, std::string timestamp,
     {
         return false;
     }
-    phosphor::network::snmp::sendTrap<
-        phosphor::network::snmp::OBMCErrorNotification>(
-        static_cast<uint32_t>(eventId), timestamp, sev, std::move(msg));
-    eventSeqNum++;
-    return true;
+    try
+    {
+        phosphor::network::snmp::sendTrap<
+            phosphor::network::snmp::OBMCErrorNotification>(
+            static_cast<uint32_t>(eventId), timestamp, sev, std::move(msg));
+        eventSeqNum++;
+        return true;
+    }
+    catch (const sdbusplus::exception_t& e)
+    {
+        BMCWEB_LOG_ERROR("Exception during SNMP trap send: {}", e.what());
+        return false;
+    }
 }
 
 void Subscription::filterAndsendSNMPTrap(
