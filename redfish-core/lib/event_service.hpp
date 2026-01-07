1d8bb6d9396ed63e63701e9115ad80d4e5cb13b3
Govardhan <govardhanc@ami.com>
2026-01-07 13:47:12 +0530

    Added the versionPatch validation

--- redfish-core/lib/event_service.hpp
+++ redfish-core/lib/event_service.hpp
@@ -656,7 +656,7 @@ bool validateMsgId(const std::string& messageId)
     std::vector<std::string> fields;
     bmcweb::split(fields, messageId, '.');
 
-    if (fields.size() == 4)
+    if (fields.size() == 5)
     {
         // MessageId Format:
         // <Registry Prefix>.<Major Version>.<Minor Version>.<MessageKey>
@@ -664,7 +664,8 @@ bool validateMsgId(const std::string& messageId)
         const std::string& msgPrefix = fields[0];
         const std::string& majorStr = fields[1];
         const std::string& minorStr = fields[2];
-        const std::string& msgSuffix = fields[3];
+        const std::string& patchStr = fields[3];
+        const std::string& msgSuffix = fields[4];
 
         const auto registry = redfish::registries::getRegistryFromPrefix(msgPrefix);
         const auto* header = redfish::registries::resolveHeader(msgPrefix);
@@ -675,7 +676,8 @@ bool validateMsgId(const std::string& messageId)
         }
 
         if (std::to_string(header->versionMajor) == majorStr &&
-            std::to_string(header->versionMinor) == minorStr)
+            std::to_string(header->versionMinor) == minorStr &&
+            std::to_string(header->versionPatch) == patchStr)
         {
             if(std::any_of(registry.begin(), registry.end(),
                             [&msgSuffix](const redfish::registries::MessageEntry& messageEntry) {
