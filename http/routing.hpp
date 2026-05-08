3bc0a057b2f67b635fe8b071078cb5e3b45f6df5
J.C Chiu (邱奕偉) <jcchiu@ami.com>
2026-05-08 03:16:28 +0000

    Revert !1145 Added exception case for TP packs for routing mechanism

--- http/routing.hpp
+++ http/routing.hpp
@@ -291,9 +291,7 @@ class Trie
     (!BMCWEB_AMI_CXL_MACRO) && (!defined(ONETREE_NVIDIASIPACK)) &&             \
     (!defined(ONETREE_GPGPU)) && (!defined(ONETREE_RM)) &&                     \
     (!defined(ONETREE_PSM)) && (!BMCWEB_AMI_THERMALEQUIPMENT_MACRO) &&         \
-    (!defined(ONETREE_ARM_SBMR) && (!defined(ONETREE_BRCMRAID)) &&             \
-     (!defined(ONETREE_BRCMRAID8)) && (!defined(ONETREE_MSCCRAID)) &&          \
-     (!defined(ONETREE_NVME)))
+    (!defined(ONETREE_ARM_SBMR))
         if (node.ruleIndex != 0U)
         {
             BMCWEB_LOG_CRITICAL("handler already exists for \"{}\"", urlIn);
