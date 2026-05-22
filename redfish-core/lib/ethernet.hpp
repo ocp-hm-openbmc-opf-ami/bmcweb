8f2f5ec64fc616f198be3fa81226e2d626bb1a39
Ginger Cheng (程怡晴) <gingercheng@ami.com>
2026-05-22 05:47:52 +0000

    OT-28630 - [OT3.1][LF_Sync] Disable non-active interface throws 500 when bond is enabled

--- redfish-core/lib/ethernet.hpp
+++ redfish-core/lib/ethernet.hpp
@@ -1937,7 +1937,8 @@ inline void handleInterfacePatch(
                                                 "InterfaceEnabled");
                 return; // Exit the entire handleInterfacePatch function
             }
-            if (ifaceId == activeSlave && *interfaceEnabled == false)
+            if (!activeSlave.empty() && ifaceId != "bond0" &&
+                *interfaceEnabled == false)
             {
                 messages::BondActiveSlaveDisable(asyncResp->res, ifaceId);
                 return; // Exit the entire handleInterfacePatch function
